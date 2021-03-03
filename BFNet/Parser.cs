cainy-a/BFNet

commit 2fc90e878271e2a23cd0ab6e40d60481fc1079ec

Remove unneeded parameter, convert to LINQ

BFNet/Parser.cs
@@ -26,20 +26,17 @@ namespace BFNet
 
 		private static HierarchyRoot ExpandHierarchy(this HierarchyRoot hierarchy)
 		{
-			var tree = new List<HierarchyObject>();
-			for (var i = 0; i < hierarchy.Hierarchy.Length; i++)
+			return new()
 			{
-				var hierarchyObject = hierarchy.Hierarchy[i];
-
-				tree.Add(((Instruction) hierarchyObject).Operation == Operations.StartLoop
-							 ? new Loop {HierarchyChildren = ExpandLoopContentRecursive(ref i, ref tree, ref hierarchy)}
-							 : hierarchyObject);
-			}
-
-			return new HierarchyRoot {Hierarchy = tree.ToArray()};
+				Hierarchy = hierarchy.Hierarchy
+									 .Select((hierarchyObject, i) => // for each item in the hierarchy
+                                          ((Instruction) hierarchyObject).Operation == Operations.StartLoop // is it a loop start? 
+															   ? new Loop {HierarchyChildren = ExpandLoopContentRecursive(ref i, ref hierarchy)} // Expand the loop (recursively!)
+															   : hierarchyObject).ToArray() // Else just copy it untouched
+			};
 		}
 
-		private static HierarchyObject[] ExpandLoopContentRecursive(ref int i, ref List<HierarchyObject> tree, ref HierarchyRoot root)
+		private static HierarchyObject[] ExpandLoopContentRecursive(ref int i, ref HierarchyRoot root)
 		{
 			var loopContent = new List<HierarchyObject>();
 			while (true)
@@ -50,7 +47,7 @@ namespace BFNet
 				{
 					loopContent.Add(new Loop
 					{
-						HierarchyChildren = ExpandLoopContentRecursive(ref i, ref tree, ref root)
+						HierarchyChildren = ExpandLoopContentRecursive(ref i, ref root)
 					});
 					continue;
 				}

commit a5de4219439c65107e2d8e8aecb2754ce9ad4cbe

Fix parser after it broke in 2fc90e87

BFNet/Parser.cs
@@ -17,16 +17,21 @@ namespace BFNet
 					   .ToArray()
 		};
 
-		private static TreeRoot ExpandTree(this TreeRoot tree)
+		private static TreeRoot ExpandTree(this TreeRoot root)
 		{
-			return new()
+			var tree = new List<TreeObject>();
+			// Actually it can't - I did that in 2fc90e87 and it broke.
+			// ReSharper disable once LoopCanBeConvertedToQuery
+			for (var i = 0; i < root.Tree.Length; i++)
 			{
-				Tree = tree.Tree
-						   .Select((hierarchyObject, i) => // for each item in the tree
-											((Instruction) hierarchyObject).Operation == Operations.StartLoop // is it a loop start? 
-												? new Loop {TreeChildren = ExpandLoopContentRecursive(ref i, ref tree)} // Expand the loop (recursively!)
-												: hierarchyObject).ToArray() // Else just copy it untouched
-			};
+				var hierarchyObject = root.Tree[i];
+
+				tree.Add(((Instruction) hierarchyObject).Operation == Operations.StartLoop
+							 ? new Loop {TreeChildren = ExpandLoopContentRecursive(ref i, ref root)}
+							 : hierarchyObject);
+			}
+
+			return new TreeRoot {Tree = tree.ToArray()};
 		}
 
 		private static TreeObject[] ExpandLoopContentRecursive(ref int i, ref TreeRoot root)