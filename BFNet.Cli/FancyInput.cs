054e5f6cb7584c0e2fd001a45692b72c8093d185
cainy-a <cainy-a@users.noreply.github.com>
2021-03-31 16:10:44 +0100

    Just need to fix cursor

--- BFNet.Cli/FancyInput.cs
+++ BFNet.Cli/FancyInput.cs
@@ -10,52 +10,56 @@ namespace BFNet.Cli
 	{
 		public static string GetInput(out int lineCount)
 		{
-			var     input = new StringBuilder();
+			var showCursor = true;
+			try
+			{
+				showCursor = Console.CursorVisible;
+			}
+			catch
+			{
+				// this only works on windows.
+			}
+			//Console.CursorVisible = false;
+
+			var                   input          = new StringBuilder();
 			IList<ConsoleKeyInfo> previousInputs = new List<ConsoleKeyInfo>();
 
 			var currentX = 0;
 			var currentY = 0;
 
-			string[] Lines() => input.ToString().Split('\n');
-			int LineLength() => Lines()[currentY].Length;
-			int LineCount()  => Lines().Length;
+			string[] Lines()      => input.ToString().Split('\n');
+			int      LineLength() => Lines()[currentY].Length;
+			int      LineCount()  => Lines().Length;
 
 			while (true)
 			{
 				var key = Console.ReadKey();
 
-				if (key.Key                            == ConsoleKey.Enter
-				 && previousInputs.LastOrDefault().Key == ConsoleKey.Enter) break;
+				if (key.Key == ConsoleKey.Enter && previousInputs.LastOrDefault().Key == ConsoleKey.Enter) break;
 
 				// ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
 				switch (key.Key)
 				{
 					case ConsoleKey.RightArrow:
-						if (currentX + 1 < LineLength())
-						{
-							currentX++;
-							Console.Out.Write(Lines()[currentY][currentX]);
-						}
+						if (currentX      + 1 < LineLength()) currentX++;
 						else if (currentY + 1 < LineCount())
 						{
-							currentX           = 0;
-							Console.CursorTop++;
+							currentX = 0;
 							currentY++;
+							Console.CursorTop++;
 						}
+
 						break;
 
 					case ConsoleKey.LeftArrow:
-						if (currentX > 0)
-						{
-							currentX--;
-							Console.Out.Write(Lines()[currentY][currentX]);
-						}
+						if (currentX      > 0) currentX--;
 						else if (currentY > 0)
 						{
 							currentY--;
 							Console.CursorTop--;
-							currentX           = LineLength() - 1;
+							currentX = LineLength() - 1;
 						}
+
 						break;
 
 					case ConsoleKey.DownArrow:
@@ -64,10 +68,11 @@ namespace BFNet.Cli
 							currentY++;
 							Console.CursorTop++;
 						}
+
 						break;
-					
+
 					case ConsoleKey.Backspace:
-						input.Remove(input.Length - 1, 1);
+						input.RemoveLast();
 						if (LineLength() > 0)
 							currentX--;
 						else
@@ -76,8 +81,9 @@ namespace BFNet.Cli
 							Console.CursorTop--;
 							currentX = LineLength() - 1;
 						}
+
 						break;
-					
+
 					case ConsoleKey.Enter:
 						input.Append('\n');
 						currentX = 0;
@@ -89,12 +95,70 @@ namespace BFNet.Cli
 						currentX++;
 						break;
 				}
-				Console.CursorLeft = Math.Min(currentX, LineLength());
+
 				previousInputs.Add(key);
+
+				Clear(LineCount());
+				ReRender(input, currentX, currentY);
 			}
 
-			lineCount = LineCount();
+
+			lineCount             = LineCount();
+			Console.CursorVisible = showCursor;
 			return input.ToString();
+		}
+		
+		private static void ReRender(StringBuilder input, int currentX, int currentY)
+		{
+			var lines = input.ToString().Split('\n');
+
+			var currentIndex = 0;
+			for (var i = 0; i + 1 < currentY; i++)
+			{
+				currentIndex += lines[currentY].Length // the line
+							  + 1;                     // the newline char
+			}
+
+			var text = lines.Select(l => l + ' ').Aggregate(string.Empty, (current, next) => current + next + '\n');
+
+			currentIndex += currentX; // account for x pos
+
+			var textBefore  = text[new Range(0, currentIndex)];
+			var currentChar = text[currentIndex];
+			var textAfter   = string.Empty;
+			if (text.Length - 1 > currentIndex + 1)
+				textAfter = text[new Range(currentIndex + 1, text.Length - 1)];
+
+			// we need these later
+			var fgCol = Console.ForegroundColor;
+			var bgCol = Console.BackgroundColor;
+
+			// start text
+			Console.Write(textBefore);
+			// current char
+			Console.BackgroundColor = fgCol;
+			Console.ForegroundColor = bgCol;
+			Console.Write(currentChar);
+			// text after
+			Console.BackgroundColor = bgCol;
+			Console.ForegroundColor = fgCol;
+			Console.Write(textAfter);
+		}
+
+		private static void Clear(int lines)
+		{
+			Console.CursorTop  -= lines - 1;
+			Console.CursorLeft =  0;
+
+			for (var i = 0; i < lines; i++)
+			{
+				var sb = new StringBuilder();
+				for (var j = 0; j < Console.BufferWidth; j++) sb.Append(' ');
+				Console.Write(sb.ToString());
+			}
+
+			Console.CursorTop  -= lines;
+			Console.CursorLeft =  0;
 		}
 	}
 }
--- BFNet.Cli/Utils.cs
+++ BFNet.Cli/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace BFNet.Cli
 {
@@ -7,5 +8,7 @@ namespace BFNet.Cli
 	{
 		public static bool Contains(this IEnumerable<char> enumerable, params char[] chars) 
 			=> chars.Any(c => Contains(enumerable, c)); // check for any characters which are contained in enumerable
+
+		public static void RemoveLast(this StringBuilder sb) => sb.Remove(sb.Length - 1, 1);
 	}
 }
